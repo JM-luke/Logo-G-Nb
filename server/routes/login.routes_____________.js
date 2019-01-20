1c46ac945557d12b0578ed772e40db22265b28f4
JM-luke <wan@ono.com>
2019-01-20 17:46:10 +0100

    actualizar desde portatil

--- server/routes/login.routes_____________.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginCtrl = require('../controllers/login.controller');
-
-router.post('/', loginCtrl.loginUser);
-
-module.exports = router;
--- server/routes/register.routes__________.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersCtrl = require('../controllers/users.controller');
-
-router.post('/', usersCtrl.createUser);
-
-module.exports = router;
